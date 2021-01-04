f4526b339d9c3b924128af82c3e536a32d697243
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2021-01-04 21:30:59 +0000

    Update RestAPIProduct.py

--- RestAPIProduct.py
+++ RestAPIProduct.py
@@ -5,7 +5,7 @@ Created on Sun Jan  3 00:31:14 2021
 @author: pauls
 """
 
-#https://github.com/andrewbeattycourseware/dataRepresenation2020/blob/master/code/week05-lecture/servers/restserver.py
+#Code adapted from https://github.com/andrewbeattycourseware/dataRepresenation2020/blob/master/code/week05-lecture/servers/restserver.py
 #!flask/bin/python
 from flask import Flask, jsonify,  request, abort, make_response
 
@@ -13,85 +13,85 @@ app = Flask(__name__,
             static_url_path='', 
             static_folder='../')
 
-cars = [
+products = [
     {
-        "reg":"181 G 1234",
-        "make":"Ford",
-        "model":"Modeo",
-        "price":18000
+        "type":"Ketchup",
+        "barcode":"56789",
+        "make":"Heinz",
+        "price":3
     },
     {
-        "reg":"11 MO 1234",
-        "make":"Nissan",
-        "model":"Almera",
-        "price":8000
+        "type":"Potatoes",
+        "barcode":"23456",
+        "make":"Ardfert",
+        "price":9
     },
     {
-        "reg":"test",
-        "make":"Nissan",
-        "model":"Almera",
-        "price":8000
+        "type":"Tomatoes",
+        "barcode":"56687",
+        "make":"OnTheVine",
+        "price":5
     }
 ]
 
-@app.route('/cars', methods=['GET'])
-def get_cars():
-    return jsonify( {'cars':cars})
-# curl -i http://localhost:5000/cars
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify( {'products':products})
+# curl -i http://localhost:5000/products
 
-@app.route('/cars/<string:reg>', methods =['GET'])
-def get_car(reg):
-    foundCars = list(filter(lambda t : t['reg'] == reg , cars))
-    if len(foundCars) == 0:
-        return jsonify( { 'car' : '' }),204
-    return jsonify( { 'car' : foundCars[0] })
-#curl -i http://localhost:5000/cars/test
+@app.route('/products/<string:reg>', methods =['GET'])
+def get_products(products):
+    foundProducts = list(filter(lambda t : t['type'] == type , products))
+    if len(foundProducts) == 0:
+        return jsonify( { 'products' : '' }),204
+    return jsonify( { 'products' : foundProducts[0] })
+#curl -i http://localhost:5000/products/test
 
-@app.route('/cars', methods=['POST'])
-def create_car():
+@app.route('/products', methods=['POST'])
+def create_products():
     if not request.json:
         abort(400)
-    if not 'reg' in request.json:
+    if not 'type' in request.json:
         abort(400)
-    car={
-        "reg":  request.json['reg'],
-        "make": request.json['make'],
-        "model":request.json['model'],
+    products={
+        "type":  request.json['type'],
+        "barcode": request.json['barcode'],
+        "make":request.json['make'],
         "price":request.json['price']
     }
-    cars.append(car)
-    return jsonify( {'car':car }),201
+    products.append(products)
+    return jsonify( {'products':products }),201
 # sample test
-# curl -i -H "Content-Type:application/json" -X POST -d '{"reg":"12 D 1234","make":"Fiat","model":"Punto","price":3000}' http://localhost:5000/cars
-# curl -i -H "Content-Type:application/json" -X POST -d "{\"reg\":\"12 D 1234\",\"make\":\"Fiat\",\"model\":\"Punto\",\"price\":3000}' http://localhost:5000/cars
+# curl -i -H "Content-Type:application/json" -X POST -d '{"type":"Ketchup","barcode":"56789","make":"Heinz","price":3}' http://localhost:5000/products
+# curl -i -H "Content-Type:application/json" -X POST -d "{\"type\":\"Ketchuo\",\"barcode\":\"56789\",\"make"\":\\"Heinz,\"price\":3}' http://localhost:5000/products
 
-@app.route('/cars/<string:reg>', methods =['PUT'])
-def update_car(reg):
-    foundCars=list(filter(lambda t : t['reg'] ==reg, cars))
-    if len(foundCars) == 0:
+@app.route('/products/<string:reg>', methods =['PUT'])
+def update_products(type):
+    foundProducts=list(filter(lambda t : t['type'] ==type, products))
+    if len(foundProducts) == 0:
         abort(404)
     if not request.json:
         abort(400)
-    if 'make' in request.json and type(request.json['make']) != str:
+    if 'barcode' in request.json and type(request.json['barcode']) != str:
         abort(400)
-    if 'model' in request.json and type(request.json['model']) is not str:
+    if 'make' in request.json and type(request.json['make']) is not str:
         abort(400)
     if 'price' in request.json and type(request.json['price']) is not int:
         abort(400)
-    foundCars[0]['make']  = request.json.get('make', foundCars[0]['make'])
-    foundCars[0]['model'] =request.json.get('model', foundCars[0]['model'])
-    foundCars[0]['price'] =request.json.get('price', foundCars[0]['price'])
-    return jsonify( {'car':foundCars[0]})
-#curl -i -H "Content-Type:application/json" -X PUT -d '{"make":"Fiesta"}' http://localhost:5000/cars/181%20G%201234
-#curl -i -H "Content-Type:application/json" -X PUT -d "{\"make\":\"Fiesta\"}" http://localhost:5000/cars/181%20G%201234
+    foundProducts[0]['barcode']  = request.json.get('barcode', foundProducts[0]['barcode'])
+    foundProducts[0]['make'] =request.json.get('make', foundProducts[0]['make'])
+    foundProducts[0]['price'] =request.json.get('price', foundProducts[0]['price'])
+    return jsonify( {'products':foundProducts[0]})
+#curl -i -H "Content-Type:application/json" -X PUT -d '{"barcode":""}' http://localhost:5000/products
+#curl -i -H "Content-Type:application/json" -X PUT -d "{\"make\":\"\"}" http://localhost:5000/products
 
 
-@app.route('/cars/<string:reg>', methods =['DELETE'])
-def delete_car(reg):
-    foundCars = list(filter (lambda t : t['reg'] == reg, cars))
-    if len(foundCars) == 0:
+@app.route('/products/<string:type>', methods =['DELETE'])
+def delete_products(type):
+    foundProducts= list(filter (lambda t : t['type'] == type, products))
+    if len(foundProducts) == 0:
         abort(404)
-    cars.remove(foundCars[0])
+    products.remove(foundProducts[0])
     return  jsonify( { 'result':True })
 
 @app.errorhandler(404)
